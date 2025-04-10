0219e1730aeeae8b7e2f2abb5528d41c6e94c51f
Sasha Le <iamsasha.le@gmail.com>
2025-04-10 10:30:08 +1000

    add heimdall to tidewallet

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -17,6 +17,23 @@ git clone https://github.com/tide-foundation/tidecloak-js.git
 cd tidecloak-js
 git checkout origin/staging
 cd ..
+git clone https://github.com/tide-foundation/heimdall.git
+cd heimdall
+git checkout origin/staging
+cd ..
+
+echo "📦 [2/11] Installing dependencies..."
+cd tide-wallet/tide-modules
+mkdir -p modules
+cp -r ../../heimdall ./modules
+tsc
+cd ../../
+tsc
+npm install
+cd ..
+cp -r tide-js ./tidecloak-js/modules/.
+cp -r tidecloak-js ./tide-wallet
+
 
 echo "🌐 [3/11] Building Codespace URLs..."
 CODESPACE_URL="https://${CODESPACE_NAME}-3000.app.github.dev"
@@ -25,4 +42,3 @@ TIDECLOAK_LOCAL_URL="https://staging.dauth.me"
 # ✍️ [4/11] Writing TIDECLOAK_LOCAL_URL to tide-wallet/.env.local
 echo "TIDECLOAK_LOCAL_URL=$TIDECLOAK_LOCAL_URL" > tide-wallet/.env.local
 echo "CODESPACE_URL=$CODESPACE_URL" > tide-wallet/.env.local
-
