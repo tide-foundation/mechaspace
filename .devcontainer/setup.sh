91d5b3c353b70e52df655bf34d5388940862cc01
Sasha Le <iamsasha.le@gmail.com>
2025-05-15 11:23:36 +1000

    deploy a tc instance and connect purse

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,64 +1,66 @@
 #!/bin/bash
 set -e
 
-CODESPACE_URL=""
-if [[ -n "$CODESPACE_NAME" ]]; then
-  CODESPACE_URL="https://${CODESPACE_NAME}-3000.app.github.dev"
-else
-  CODESPACE_URL="http://localhost:3000"
-fi
-TIDECLOAK_LOCAL_URL="https://staging.dauth.me"
-TIDE_THRESHOLD_T=3
-TIDE_THRESHOLD_N=5
+CODESPACE_URL="https://${CODESPACE_NAME}-3000.app.github.dev"
+CODESPACE_URL_TC="https://${CODESPACE_NAME}-8080.app.github.dev"
 
-echo "🔧 [0/11] Installing required dependencies (OpenSSL)..."
+# CODESPACE_URL=""
+# if [[ -n "$CODESPACE_NAME" ]]; then
+#   CODESPACE_URL="https://${CODESPACE_NAME}-3000.app.github.dev"
+# else
+#   CODESPACE_URL="http://localhost:3000"
+# fi
+# TIDECLOAK_LOCAL_URL="https://staging.dauth.me"
+# TIDE_THRESHOLD_T=3
+# TIDE_THRESHOLD_N=5
+
+echo "🔧 [0/4] Installing required dependencies (OpenSSL)..."
 sudo apt-get update -y
 sudo apt-get install -y libssl-dev
 
-echo "🚀 [1/11] Cloning the MECHAPURSE..."
+echo "🐳 [1/4] Pulling and starting Tidecloak container..."
+docker pull docker.io/tideorg/tidecloak-dev:latest
+docker run -d \
+  --name tidecloak \
+  -p 8080:8080 \
+  -e KC_HOSTNAME=${CODESPACE_URL_TC} \
+  -e KC_BOOTSTRAP_ADMIN_USERNAME=admin \
+  -e KC_BOOTSTRAP_ADMIN_PASSWORD=password \
+  tideorg/tidecloak-dev:latest
+
+echo ""
+echo "✅ Tidecloak Setup complete."
+echo ""
+
+echo "🚀 [2/4] Cloning the MECHAPURSE..."
 
 # Clean up old clones if they exist
 [ -d mechapurse ] && rm -rf mechapurse
-[ -d tide-js ] && rm -rf tide-js
-[ -d tidecloak-js ] && rm -rf tidecloak-js
+# [ -d tide-js ] && rm -rf tide-js
+# [ -d tidecloak-js ] && rm -rf tidecloak-js
 [ -d heimdall ] && rm -rf heimdall
 
 # Clone the repositories
 git clone https://github.com/tide-foundation/mechapurse.git
 
 # TEMPORARY UNTIL WE HAVE STUFF TO PROD!!
-git clone https://github.com/tide-foundation/tide-js.git
-cd tide-js
-git checkout origin/staging
-cd ..
-
-git clone https://github.com/tide-foundation/tidecloak-js.git
-cd tidecloak-js
-git checkout origin/staging
-cd ..
-
 git clone https://github.com/tide-foundation/heimdall.git
 cd heimdall
 git checkout origin/staging
 cd ..
 
-echo "📦 [2/11] Installing dependencies..."
-cd mechapurse/tide-modules
-mkdir -p modules
-cp -r ../../heimdall/src/heimdall.js ./modules
-tsc
-cd ..
+echo "📦 [3/4] Installing dependencies..."
+cd mechapurse
 npm install
 cd ..
 
-# Update threshold values in tide-js
-sed -i "s/\(export const Threshold = \)[0-9]\+;/\1${TIDE_THRESHOLD_T};/" ./tide-js/Tools/Utils.js
-sed -i "s/\(export const Max = \)[0-9]\+;/\1${TIDE_THRESHOLD_N};/" ./tide-js/Tools/Utils.js
 
-# Copy modules to tidecloak-js and mechapurse
-cp -r tide-js ./tidecloak-js/modules/.
-cp -r tidecloak-js ./mechapurse/node_modules/.
-
-# ✍️ [3/3] Writing environment variables to mechapurse/.env.local
-echo "TIDECLOAK_LOCAL_URL=$TIDECLOAK_LOCAL_URL" > mechapurse/.env.local
+echo "✍️ [4/4] Writing environment variables to mechapurse/.env.local"
+echo "TIDECLOAK_LOCAL_URL=$CODESPACE_URL_TC" > mechapurse/.env.local
 echo "CODESPACE_URL=$CODESPACE_URL" >> mechapurse/.env.local
+
+echo ""
+echo "✅ Mechapurse Setup complete."
+echo ""
+
+
