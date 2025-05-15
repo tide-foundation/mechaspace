072a20f46497448762ce5f707ac0a217eeebb978
Sasha Le <iamsasha.le@gmail.com>
2025-05-15 18:13:08 +1000

    fix scripts

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,16 +4,6 @@ set -e
 CODESPACE_URL="https://${CODESPACE_NAME}-3000.app.github.dev"
 CODESPACE_URL_TC="https://${CODESPACE_NAME}-8080.app.github.dev"
 
-# CODESPACE_URL=""
-# if [[ -n "$CODESPACE_NAME" ]]; then
-#   CODESPACE_URL="https://${CODESPACE_NAME}-3000.app.github.dev"
-# else
-#   CODESPACE_URL="http://localhost:3000"
-# fi
-# TIDECLOAK_LOCAL_URL="https://staging.dauth.me"
-# TIDE_THRESHOLD_T=3
-# TIDE_THRESHOLD_N=5
-
 echo "🔧 [0/4] Installing required dependencies (OpenSSL)..."
 sudo apt-get update -y
 sudo apt-get install -y libssl-dev
@@ -23,9 +13,9 @@ docker pull docker.io/tideorg/tidecloak-dev:latest
 docker run -d \
   --name tidecloak \
   -p 8080:8080 \
-  -e KC_HOSTNAME=${CODESPACE_URL_TC} \
-  -e KC_BOOTSTRAP_ADMIN_USERNAME=admin \
-  -e KC_BOOTSTRAP_ADMIN_PASSWORD=password \
+  -e KC_HOSTNAME="${CODESPACE_URL_TC}" \
+  -e KC_BOOTSTRAP_ADMIN_USERNAME="admin" \
+  -e KC_BOOTSTRAP_ADMIN_PASSWORD="password" \
   tideorg/tidecloak-dev:latest
 
 echo ""
@@ -35,32 +25,38 @@ echo ""
 echo "🚀 [2/4] Cloning the MECHAPURSE..."
 
 # Clean up old clones if they exist
-[ -d mechapurse ] && rm -rf mechapurse
-# [ -d tide-js ] && rm -rf tide-js
-# [ -d tidecloak-js ] && rm -rf tidecloak-js
-[ -d heimdall ] && rm -rf heimdall
+REPOS=("mechapurse" "heimdall")
+for repo in "${REPOS[@]}"; do
+  [ -d "$repo" ] && rm -rf "$repo"
+done
 
 # Clone the repositories
 git clone https://github.com/tide-foundation/mechapurse.git
-
-# TEMPORARY UNTIL WE HAVE STUFF TO PROD!!
-git clone https://github.com/tide-foundation/heimdall.git
-cd heimdall
-git checkout origin/staging
-cd ..
 
 echo "📦 [3/4] Installing dependencies..."
 cd mechapurse
 npm install
 cd ..
 
+echo "🚧 [INFO] Cloning and setting up Heimdall (temporary)"
+git clone https://github.com/tide-foundation/heimdall.git
+cd heimdall
+git checkout origin/staging
+cd ..
+cd mechapurse/tide-modules
+mkdir -p modules
+cp -r ../../heimdall/src/heimdall.js ./modules
+tsc
+cd ..
+npm install
+cd ..
 
 echo "✍️ [4/4] Writing environment variables to mechapurse/.env.local"
-echo "TIDECLOAK_LOCAL_URL=$CODESPACE_URL_TC" > mechapurse/.env.local
-echo "CODESPACE_URL=$CODESPACE_URL" >> mechapurse/.env.local
+cat <<EOF > mechapurse/.env.local
+TIDECLOAK_LOCAL_URL=${CODESPACE_URL_TC}
+CODESPACE_URL=${CODESPACE_URL}
+EOF
 
 echo ""
 echo "✅ Mechapurse Setup complete."
 echo ""
-
-
